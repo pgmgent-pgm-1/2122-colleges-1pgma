9cca74ad7d8445abf0251c943ecc2c4b5c10611d
Philippe De Pauw - Waterschoot <drdynscript>
2021-10-21 10:59:24 +0200

    Added generateUI function to the app

--- week5/recap/js/main.js
+++ week5/recap/js/main.js
@@ -12,14 +12,14 @@
       'playDay': 'Vrijdag'
     },
     {
-      'thumbnailUrl': 'https://assets.rockwerchter.be/files/cache/medium/files/fnm-1500-60cc6b19a9efc.jpg',
-      'bandName': 'Faith No More',
-      'playDay': 'Zaterdag'
+      'thumbnailUrl': 'https://assets.rockwerchter.be/files/cache/medium/files/14686-pearl-jam-032020-6c9a9964-flat-lowres-5f1a952633f63.jpg',
+      'bandName': 'Pearl Jam',
+      'playDay': 'Donderdag'
     },
     {
-      'thumbnailUrl': 'https://assets.rockwerchter.be/files/cache/medium/files/metallica-artiest-60c85d0413a36.jpg',
-      'bandName': 'Metallica',
-      'playDay': 'Vrijdag'
+      'thumbnailUrl': 'https://assets.rockwerchter.be/files/cache/medium/files/rhcp-web-61543fbab0b59.jpg',
+      'bandName': 'Red Hot Chilly Peppers',
+      'playDay': 'Zondag'
     }
   ];
   // Define the application
@@ -34,6 +34,11 @@
       // Get the node or element via querySelector with certain selectors
       this.lineupElement = document.querySelector('.lineup');
       console.log(this.lineupElement);
+      // Call the function generateUI
+      this.generateUI();
+    },
+    generateUI() {
+      console.log('3. Generate User Interface!')
       // Read the lineup data, convert it to an html string and inject it to the lineupElement via the property innerHTML
       // this.lineupElement.innerHTML = lineup.map((concert) => `
       // <div class="concert">
