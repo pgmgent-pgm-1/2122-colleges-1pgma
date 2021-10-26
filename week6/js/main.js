e31de6deca7ca121d036ac19775496a4e1902370
drdynscript <drdynscript@gmail.com>
2021-10-26 10:02:54 +0200

    Added students example

--- week6/js/main.js
+++ week6/js/main.js
@@ -54,6 +54,13 @@
       console.log('2. Cache DOM elements!');
       // Cache the div element with class value students
       this.$studentsElement = document.querySelector('.students');
+      // Cache the div element with class value btn-backgound
+      this.$btnBackgroundElement = document.querySelector('.btn-backgound');
+      // Add event listener to the previous element
+      this.$btnBackgroundElement.addEventListener('click', (ev) => {
+        document.body.style.backgroundColor = `hsla(${Math.round(Math.random() * 360)}, 100%, 50%, 1)`;
+        document.body.style.transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+      }, false);
     },
     generateUI () {
       console.log('3. Generate UI!');
