c38b939c594c23b60e40845039ae24c7c784be49
drdynscript <drdynscript@gmail.com>
2021-10-28 10:00:19 +0200

    Refactored the application

--- week6/wicked/js/main.js
+++ week6/wicked/js/main.js
@@ -36,11 +36,12 @@
       //   document.body.style.backgroundColor = `hsl(${ev.offsetX}, 100%, 50%)`;
       //   document.body.style.transform = `rotate(${ev.offsetY}deg)`;
       // });
+
       document.body.addEventListener('mousemove', (ev) => {
-        // Get dimension window
+        // Get dimension of tag-cloud element
         const wH = this.$tagCloud.clientHeight - 200;
         const wW = this.$tagCloud.clientWidth - 200;
-        // Get all tag elements from the tag-cloud
+        // Get all the tag elements from the tag-cloud
         const $tags = document.querySelectorAll('.tag-cloud .tag');
         $tags.forEach(($tag) => {
           $tag.style.color = `hsl(${ev.offsetY}, 100%, 50%)`;
@@ -50,10 +51,14 @@
     },
     generateUI () {
       console.log('3. Generate User Interface.');
+      // Generate UI for tags
+      this.$tagCloud.innerHTML = this.getHTMLStringForTags(tags);
+    },
+    getHTMLStringForTags (tags) {
       const wH = this.$tagCloud.clientHeight - 200;
       const wW = this.$tagCloud.clientWidth - 200;
-      
-      this.$tagCloud.innerHTML = tags.map(tag => {
+
+      return tags.map(tag => {
         return `<div class="tag" style="transform: translateX(${Math.round(Math.random() * wW)}px) translateY(${Math.round(Math.random() * wH)}px); font-size: ${tag.length / 2}rem">${tag}</div>`
       }).join('');
     },
